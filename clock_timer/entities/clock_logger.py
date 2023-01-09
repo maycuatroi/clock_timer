4ddf36b755d722c4aaf481373dd7a5f9d0d0d471
Nguyễn Anh Bình <sometimesocrazy@gmail.com>
2023-01-10 02:18:36 +0700

    sync code, add log call back

--- clock_timer/entities/clock_logger.py
+++ clock_timer/entities/clock_logger.py
@@ -2,12 +2,13 @@ import time
 
 
 class ClockLogger:
-    def __init__(self, logger_function=None):
+    def __init__(self, logger_function=None, log_callback=None):
         self.log = {}
         if logger_function is not None:
             self.logger_function = logger_function
         else:
             self.logger_function = print
+        self.log_callback = log_callback
         self.start = None
         self.end = None
         self.elapsed = None
@@ -18,13 +19,29 @@ class ClockLogger:
             result = func(*args, **kwargs)
             end = time.perf_counter()
             elapsed = end - start
-            if func.__name__ not in self.log:
-                self.log[func.__name__] = [0, 0]
-            self.log[func.__name__][0] += 1
-            self.log[func.__name__][1] += elapsed
+            function_name = func.__name__
+            args = [str(arg) for arg in args]
+            kwargs = [f"{key}={value}" for key, value in kwargs.items()]
+            args = ", ".join(args)
+            kwargs = ", ".join(kwargs)
+
+            if function_name not in self.log:
+                self.log[function_name] = []
+            self.log[function_name].append((args, kwargs, elapsed))
+            call_count = len(self.log[function_name])
             self.logger_function(
-                f"{func.__name__} was called {self.log[func.__name__][0]} times, total time elapsed: {self.log[func.__name__][1]:.6f}s"
+                f"{function_name} was called {call_count} times, "
+                f"total time elapsed: {elapsed:.3f}s"
             )
+            if self.log_callback:
+                self.log_callback(
+                    function_name=function_name,
+                    call_count=call_count,
+                    elapsed_time=elapsed,
+                    function_args=args,
+                    function_kwargs=kwargs,
+                )
+
             return result
 
         return wrapper
@@ -37,3 +54,12 @@ class ClockLogger:
         self.end = time.perf_counter()
         self.elapsed = self.end - self.start
         self.logger_function(f"Total time elapsed: {self.elapsed:.6f}s")
+
+    def __str__(self):
+        if self.log:
+            for key, value in self.log.items():
+                self.logger_function(
+                    f"{key} was called {value[0]} times, total time elapsed: {value[1]:.6f}s"
+                )
+        else:
+            self.logger_function(f"Total time elapsed: {self.elapsed:.6f}s")
